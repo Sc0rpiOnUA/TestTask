b5855720274dc75d3b3b61fc753c4db8774a0a7a
CattusGames <cattusgames@gmail.com>
2022-10-27 02:17:06 +0300

    Set up animations

--- Assets/ZombieAI.cs
+++ Assets/ZombieAI.cs
@@ -10,7 +10,6 @@ public class ZombieAI : MonoBehaviour
     private Collider[] triggerRange;
     [SerializeField] ParticleSystem _boomParticle;
     private Animator anim;
-
     // Start is called before the first frame update
     void Start()
     {
